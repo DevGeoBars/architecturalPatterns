e5224efa27a97002953feaa037b0f5cd364de321
geo bars <georgiybarsegian@yandex.com>
2026-07-02 18:21:08 +0300

    shared / routes: -добавить константы роутов и README.md для слоя shared

---
+++ src/shared/routes/index.ts
@@ -0,0 +1,9 @@
+export const APP_ROUTES = {
+    LOGIN: '/login',
+    HOME: '/home',
+    ISSUES: '/issues',
+    CLAIMS: '/claims',
+    ISSUE_EDIT: '/issue/edit/:id',
+    ISSUE_DETAIL: '/issue/:id',
+    ISSUE_NEW: '/issue',
+} as const;
