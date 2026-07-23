68766f8d0e4e4f81aa9a514479fdf5a38b25aab3
geo bars <georgiybarsegian@yandex.com>
2026-07-23 20:25:50 +0300

    облегчить  publicApi для feature view-issues-list

--- src/features/view-issues-list/index.ts
+++ src/features/view-issues-list/index.ts
@@ -1,17 +1,3 @@
 export {
-  IssuesListStoreProvider,
-} from './model/IssuesListStoreProvider';
-
-export {
-  useIssuesListStore,
-} from './model/useIssuesListStore';
-
-export type {
-  IIssuesListState,
-  TIssuesListRequestStatus,
-  TIssuesListStore,
-} from './model/createIssuesListStore';
-
-export {
   IssuesListWidget,
 } from './ui/IssuesListWidget';
