c48867175e5d43bd5af94c60f4d55a1372a5792c
geo bars <georgiybarsegian@yandex.com>
2026-08-14 18:59:54 +0300

    server: -добавить пользователя

--- mock-server/data/users.mjs
+++ mock-server/data/users.mjs
@@ -44,6 +44,32 @@ export const users = [
         ClaimsActivity: 1,
         IsForApproveAction: false,
     },
+
+    {
+        Id: '3',
+        Guid: 'e1b76f79-c52c-4b6c-83c2-c05bf3f9c003',
+        Name: 'Сергей Кузнецов',
+        Email: 'sergey.kuznetsov@example.com',
+        IsExternal: false,
+
+        Company: {
+            Id: 1,
+            Guid: 'f4cf2a25-d229-4c07-a9a9-b53a7e1a0001',
+            Name: 'Топ Системы',
+        },
+
+        TFLEXUser: {
+            Id: 'tflex-user-3',
+            Guid: '73c25c25-aebb-420a-a794-ff246ac10003',
+            Name: 'Кузнецов Сергей Александрович',
+        },
+
+        Role: 'User',
+        UsersLimit: 0,
+        Administrator: 'Иван Иванов',
+        ClaimsActivity: 0,
+        IsForApproveAction: false,
+    },
 ];
 
 export const credentials = [
@@ -57,11 +83,15 @@ export const credentials = [
         login: 'customer',
         password: 'customer',
     },
+    {
+        userId: '3',
+        login: 'user',
+        password: 'user',
+    },
 ];
 
-
 export const subUserIdsByUserId = {
-    1: ['2'],
+    1: ['2', '3'],
 };
 
 export const userLabels = [
