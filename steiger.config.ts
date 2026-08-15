53e9b765765ce59c1cf5e1d4e86fc7ac202f46f4
geo bars <georgiybarsegian@yandex.com>
2026-08-15 23:46:11 +0300

    steiger: -добавить в исключение папку assets

--- steiger.config.ts
+++ steiger.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from 'steiger';
 
 export default defineConfig([
   ...fsd.configs.recommended,
+  {
+    files: ['./src/shared/assets/**'],
+    rules: {
+      'fsd/public-api': 'off',
+      'fsd/segments-by-purpose': 'off',
+    },
+  },
 ]);
